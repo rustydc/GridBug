7f84116e85774b2982a36103f766c679e0da7239
Rusty Mellinger <rusty.mellinger@gmail.com>
2025-04-22 02:20:03 +0000

    Add tanstack devtools.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { CssBaseline } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ root.render(
     <QueryClientProvider client={queryClient}>
       <CssBaseline />
       <App />
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition='bottom-left'/>
     </QueryClientProvider>
   </React.StrictMode>
 );
